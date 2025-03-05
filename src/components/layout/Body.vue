8b46b5fdd9bc1eda1613fcce301c33625355adc1
Luiz Fernando Hanauer <luizhanauer@gmail.com>
2025-03-05 17:54:21 -0300

    fix images

--- src/components/layout/Body.vue
+++ src/components/layout/Body.vue
@@ -15,16 +15,16 @@ export default {
   data() {
     return {
       products: [
-        { id: 1, name: 'Produto 1', price: 10, image: '/src/assets/product.jpg', quantity: 1 },
-        { id: 2, name: 'Produto 2', price: 20, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 3, name: 'Produto 3', price: 30, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 4, name: 'Produto 4', price: 40, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 5, name: 'Produto 5', price: 50, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 6, name: 'Produto 6', price: 60, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 7, name: 'Produto 7', price: 70, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 8, name: 'Produto 8', price: 80, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 9, name: 'Produto 9', price: 90, image: '/src/assets/product.jpg', quantity: 0 },
-        { id: 10, name: 'Produto 10', price: 100, image: '/src/assets/product.jpg', quantity: 0 }
+        { id: 1, name: 'Produto 1', price: 10, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 1 },
+        { id: 2, name: 'Produto 2', price: 20, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 3, name: 'Produto 3', price: 30, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 4, name: 'Produto 4', price: 40, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 5, name: 'Produto 5', price: 50, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 6, name: 'Produto 6', price: 60, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 7, name: 'Produto 7', price: 70, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 8, name: 'Produto 8', price: 80, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 9, name: 'Produto 9', price: 90, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 },
+        { id: 10, name: 'Produto 10', price: 100, image: 'https://raw.githubusercontent.com/luizhanauer/OrderAssistant/refs/heads/main/src/assets/product.jpg', quantity: 0 }
       ]
     };
   },
